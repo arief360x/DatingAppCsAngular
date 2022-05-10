d74f208c7be7db773b8fcd84079d82fdaefa07d7
Arief D <arief360x@gmail.com>
2022-05-10 14:08:29 +0800

    add skeleton for angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  //title = 'client';
+
+
+  title = 'The Dating App'
+  users: any;
+
+  //DI
+  constructor(private http: HttpClient) { }
+
+  //Http service for get
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  
 }
