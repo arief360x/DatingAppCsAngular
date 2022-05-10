3157a837eb87fea1f7134e804fc8bfadb815e66e
Arief D <arief360x@gmail.com>
2022-05-11 00:05:53 +0800

    add dto for username and password

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    // for username and password
+    public class RegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
