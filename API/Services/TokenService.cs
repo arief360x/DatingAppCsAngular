5cd50f99ea0790cc360150a2f9de733bd6e48328
Arief D <arief360x@gmail.com>
2022-05-11 23:43:08 +0800

    token interface, token class, add service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,13 @@
+﻿using API.Interfaces;
+using API.Model;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
