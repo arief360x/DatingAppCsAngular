42ef8f81e78c2a0639ccdb869af0349160e449cf
Arief D <arief360x@gmail.com>
2022-05-18 22:36:34 +0800

    add member component list and detail

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,13 +10,17 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
