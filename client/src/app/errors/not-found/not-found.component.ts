b26135d4a902957bd9b5b48b91c5925564e6c71f
Arief D <arief360x@gmail.com>
2022-05-26 01:18:03 +0800

    not found page, if login go to matches, else go to homepage

--- client/src/app/errors/not-found/not-found.component.ts
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
+  styleUrls: ['./not-found.component.css'],
 })
 export class NotFoundComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor(public accountService: AccountService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
