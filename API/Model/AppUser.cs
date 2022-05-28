4d2693b2cf9197eaf81284fe720232ced236a489
Arief D <arief360x@gmail.com>
2022-05-28 16:21:49 +0800

    extending appuser db

--- API/Model/AppUser.cs
+++ API/Model/AppUser.cs
@@ -1,4 +1,6 @@
-﻿namespace API.Model
+﻿using System.Collections;
+
+namespace API.Model
 {
     public class AppUser
     {
@@ -9,6 +11,28 @@
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive  { get; set; } 
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photots { get; set; }
 
     }
 }
