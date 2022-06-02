c166f2da6d769e65552df1f988906f8f37c748e8
Arief D <arief360x@gmail.com>
2022-06-02 16:30:15 +0800

    angular, add member interface

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,22 @@
+export interface Member {
+  id: number;
+  photoUrl: string;
+  userName: string;
+  age: number;
+  knownAs: string;
+  created: Date;
+  lastActive: Date;
+  gender: string;
+  introduction: string;
+  lookingFor: string;
+  interests: string;
+  city: string;
+  country: string;
+  photos: Photo[];
+}
+
+export interface Photo {
+  id: number;
+  url: string;
+  isMain: boolean;
+}
